d8b06760f7f93ee85bb1fcc9e040a68a6438586d
madsies <madsiesss@gmail.com>
2023-07-30 09:38:00 +0100

    base

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+class Main {
+
+    static WindowController window;
+    public static void main(String[] args) {
+        window = new WindowController();
+    }
+}
---
+++ src/WindowController.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class WindowController {
+
+    private Window window;
+    private Frame frame;
+
+    public WindowController(){
+        frame = new Frame();
+        frame.setTitle("meme companies");
+    }
+}
