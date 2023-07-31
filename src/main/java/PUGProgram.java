b941897bfbb5f5ef1c6321c9519b6e8db6b5a097
madsies <madsiesss@gmail.com>
2023-07-31 05:58:29 +0100

    Added useful comments to main

--- src/main/java/PUGProgram.java
+++ src/main/java/PUGProgram.java
@@ -1,32 +1,51 @@
 
 class PUGProgram {
 
-    private WindowController window;
-    private AppController controller;
+    private final WindowController window;
+    private final AppController controller;
     private boolean active = true;
 
     public static void main(String[] args) {
         new PUGProgram();
 
     }
+
+    /*
+    Startup logic, before window etc.
+     */
     PUGProgram(){
-        window = new WindowController();
+        // All controller functions here
         controller = new AppController();
 
         controller.createPlayer("JohnnyG", 9001, true, false, false);
         controller.createPlayer("super", 6969, false, true, true);
 
+        // All window functions here
+        window = new WindowController();
+
+        System.out.println("Startup Completed...");
         mainLoop();
         shutDown();
     }
 
+    /*
+    Updated constantly, checks for values etc.
+     */
     private void mainLoop(){
         while (active){
+            // Controller Functions
+
+            //Window Functions
             window.update();
+
+
             active = window.isActive();
         }
     }
 
+    /*
+    Code to be run at end of program, mostly saving data, etc.
+     */
     private void shutDown(){
         controller.savePlayers();
         System.out.println("Shutting down... ");
